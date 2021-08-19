40bb32ebde680353c5aba8904cf267d667a154e2
su2333066 <gisu130@gmail.com>
2021-08-19 11:14:40 +0900

    첫 작업

--- src/com/sbs/example/textBoard/Main.java
+++ src/com/sbs/example/textBoard/Main.java
@@ -15,7 +15,7 @@ public class Main {
 			String command = sc.nextLine().trim();
 			
 			if(command.equals("system exit")) {
-				System.out.println("프로그램을 종료합니다.");
+				System.out.println("프로그램을 종료합니다. ");
 				break;
 			}
 		}
